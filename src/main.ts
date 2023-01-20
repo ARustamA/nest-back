223c3e60bde01903278bfe8b444144f3cdef2ddb
“ARustamA” <rakhmetyanov@gmail.com>
2023-01-20 18:25:43 +0600

    9урок

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port');
+
+  const config = new DocumentBuilder()
+    .setTitle('lesson api')
+    .setDescription('this api for lesson')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(port);
 }
 bootstrap();
