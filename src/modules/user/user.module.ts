e04f95c73b66bcf47446a9489403ca67b0ea63b3
“ARustamA” <rakhmetyanov@gmail.com>
2023-02-19 12:59:29 +0600

    d

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,3 +1,5 @@
+import { TokenModule } from './../token/token.module';
+import { WatchList } from 'src/modules/watchlist/models/watchlist.model';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
@@ -5,7 +7,7 @@ import { UserService } from './user.service';
 import { User } from './models/user.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([User])],
+  imports: [SequelizeModule.forFeature([User, WatchList]), TokenModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService]
